ef9d3722c8731cb9feefeddc088d5fd5365c01db
Nanco Lin <nancolin92@gmail.com>
2019-06-15 23:50:10 +0200

    Use PHPMailer for SMTP on contact page

--- php/contact.php
+++ php/contact.php
@@ -1,5 +1,10 @@
 <?php
 
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
 $response = [
   'error' => null,
   'success' => false,
@@ -19,30 +24,37 @@ elseif (preg_match('/[\r\n]/', $_POST['subject'])) {
   $response['error'] = 'Invalid subject, cannot contain new line.';
 }
 else {
-  $data = [
-    'userdata' => $_POST['name'].' <'.$_POST['email'].'>',
-    'from' => $_POST['name'].' <contact@nancolin.nl>',
-    'sender' => 'Bold Design Contact <contact@nancolin.nl>',
-    'subject' => $_POST['subject'],
-    'message' => quoted_printable_encode($_POST['message']),
-  ];
+  try {
+    // Load Composer's autoloader
+    require 'vendor/autoload.php';
 
-  $headers = [
-    'MIME-Version: 1.0',
-    'Content-Type: text/plain; charset=utf-8',
-    'Content-Transfer-Encoding: quoted-printable',
-    'From: '.$data['from'],
-    'Sender: '.$data['sender'],
-    'Reply-To: '.$data['userdata'],
-  ];
+    // Instantiation and passing `true` enables exceptions
+    $mail = new PHPMailer(true);
 
-  /* Windows servers only! */
-  // $data['message'] = str_replace("\n.", "\n..", $data['message']);
+    // Server settings
+    $mail->SMTPDebug = 0;                          // Enable verbose debug output
+    $mail->isSMTP();                               // Set mailer to use SMTP
+    $mail->Host       = 'mail.gandi.net';          // Specify main and backup SMTP servers
+    $mail->SMTPAuth   = true;                      // Enable SMTP authentication
+    $mail->Username   = 'contact@nancolin.nl';     // SMTP username
+    $mail->Password   = 'f7qwqfwiUH99VJh';         // SMTP password
+    $mail->SMTPSecure = 'ssl';                     // Enable TLS encryption, `ssl` also accepted
+    $mail->Port       = 465;                       // TCP port to connect to
 
-  $response['success'] = mail('nancolin92@gmail.com', $data['subject'], $data['message'], implode("\r\n", $headers));
+    // Recipients
+    $mail->setFrom('contact@nancolin.nl', $_POST['name']);
+    $mail->addAddress('nancolin92@gmail.com');     // Add a recipient
+    $mail->addReplyTo($_POST['email'], $_POST['name']);
 
-  if (!$response['success']) {
-    $response['error'] = error_get_last()['message'];
+    // Content
+    $mail->isHTML(false); // Set email format to NOT HTML
+    $mail->Subject = $_POST['subject'];
+    $mail->Body = $_POST['message'];
+
+    $mail->send();
+    $response['success'] = true;
+  } catch (Exception $e) {
+    $response['error'] = "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
   }
 }
 
